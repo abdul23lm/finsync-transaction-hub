5ad7baef32954e2a86d6fd567a915d3793e7e912
abdul23lm <abdul23lm@gmail.com>
2024-08-12 23:16:30 +0700

    feat(dashboard): setup sidebar link constants

---
+++ constants/index.ts
@@ -0,0 +1,22 @@
+export const sidebarLinks = [
+  {
+    iconURL: "/icons/ic-home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    iconURL: "/icons/ic-dollar-circle.svg",
+    route: "/my-banks",
+    label: "My Banks",
+  },
+  {
+    iconURL: "/icons/ic-transaction.svg",
+    route: "/transaction-history",
+    label: "Transaction History",
+  },
+  {
+    iconURL: "/icons/ic-money-send.svg",
+    route: "/payment-transfer",
+    label: "Transfer Funds",
+  },
+];
